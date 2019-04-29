aac2b79af9f364fcc852e814ed0fde2948ed49eb
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-04-29 18:02:47 +0300

    adding `PhantomData` example usage

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod box_examples;
 mod mem_examples;
+mod phantom_data_examples;
 mod ptr_examples;
 mod vec_examples;
---
+++ src/phantom_data_examples.rs
@@ -0,0 +1,21 @@
+#[test]
+fn test_phantom_data() {
+    use std::marker::PhantomData;
+
+    struct MyStruct<'a, T: 'a> {
+        items: *const T,
+        phantom: PhantomData<&'a T>,
+    }
+
+    let vec = vec![10, 20, 30];
+    let items_ptr: *const u8 = vec.as_ptr();
+
+    let instance = MyStruct {
+        items: items_ptr,
+        phantom: PhantomData,
+    };
+
+    assert_eq!(10, unsafe { std::ptr::read(instance.items) });
+    assert_eq!(20, unsafe { std::ptr::read(instance.items.add(1)) });
+    assert_eq!(30, unsafe { std::ptr::read(instance.items.add(2)) });
+}
