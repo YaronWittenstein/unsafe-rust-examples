26c7ff498e71357f0d6023b7f34f4281374ab427
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-04-29 22:25:45 +0300

    adding `unsafe trait` example

--- src/misc_examples.rs
+++ src/misc_examples.rs
@@ -6,3 +6,38 @@ fn test_unsafe_cell() {
 
     assert_eq!(10, cell.into_inner());
 }
+
+#[test]
+fn test_unsafe_trait() {
+    unsafe trait DoWork {
+        fn do_safe_work(&mut self, x: i32) -> i32;
+        unsafe fn do_unsafe_work(&mut self, x: i32) -> i32;
+    }
+
+    struct Worker {
+        work_done: i32,
+    }
+
+    unsafe impl DoWork for Worker {
+        fn do_safe_work(&mut self, x: i32) -> i32 {
+            self.work_done += x;
+            self.work_done
+        }
+
+        unsafe fn do_unsafe_work(&mut self, x: i32) -> i32 {
+            self.work_done += x;
+            self.work_done
+        }
+    }
+
+    let mut w = Worker { work_done: 0 };
+
+    w.do_safe_work(10);
+    assert_eq!(10, w.work_done);
+
+    unsafe {
+        w.do_unsafe_work(20);
+    }
+
+    assert_eq!(30, w.work_done);
+}
